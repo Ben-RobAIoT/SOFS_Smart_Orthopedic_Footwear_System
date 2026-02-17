ec475bb81d3c983f440faf6af2b5236fd670e68b
Beniot Phan <beniot.robaiot1137@gmail.com>
2026-02-17 14:24:00 +0700

    Add input/output comments in MAC_Scanning.ino
    Add comments to specify input and output details for MAC scanning.

--- ESP32-C3-SuperMini/ESP-Now Protocol/MAC_Scanning.ino
+++ ESP32-C3-SuperMini/ESP-Now Protocol/MAC_Scanning.ino
@@ -1,3 +1,10 @@
+/*
+INPUT: ESP32-C3-SuperMini plug in by USB Type-C
+OUTPUT: Print out the MAC Address
+  MAC 1: Địa chỉ MAC của board này là: 58:8C:81:B1:16:AC
+  MAC 2: Địa chỉ MAC của board này là: 58:8C:81:B1:FD:44
+*/
+
 #include "WiFi.h"
 
 void setup() {
